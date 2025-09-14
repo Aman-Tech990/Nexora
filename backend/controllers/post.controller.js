ba3ab2da2994d64c27eb8ac2621be90595eed524
Aman Parida <amantechz467@gmail.com>
2025-09-14 15:52:55 +0530

    Created New Post Func inside Post Controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -5,87 +5,48 @@ import { User } from '../models/user.model.js';
 
 export const addNewPost = async (req, res) => {
     try {
-        const { caption } = req.body;
-        const image = req.file.path;
         const authorId = req.id;
+        const { caption } = req.body;
+        const image = req.file;
         if (!image) {
             return res.status(400).json({
                 success: false,
-                message: "Image are required!"
+                message: "Image is required!"
             });
         }
-
         const optimizedImageBuffer = await sharp(image.buffer)
             .resize({ width: 800, height: 800, fit: 'inside' })
-            .jpeg({ quality: 80 })
+            .toFormat('jpeg', { quality: 80 })
             .toBuffer();
 
-        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`;
-        const cloudResponse = await cloudinary.uploader.upload(fileUri, { folder: "posts" });
-        const newPost = new Post({
+        // Buffer to DataURI
+        const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString('base64')}`
+        const cloudResponse = await cloudinary.uploader.upload(fileUri);
+        const post = await Post.create({
             caption,
             image: cloudResponse.secure_url,
             author: authorId
         });
+
         const user = await User.findById(authorId);
         if (user) {
-            user.posts.push(newPost._id);
+            user.posts.push(post._id);
             await user.save();
         }
-        await newPost.populate({ path: "author", select: "-password" });
-        await newPost.save();
+        await post.populate({ path: "author", select: "-password" });
+        await post.save();
 
-        return res.status(201).json({
-            success: true,
-            message: "Post added successfully!",
-            post: newPost
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to add post!"
-        });
-    }
-}
-
-export const allPost = async (req, res) => {
-    try {
-        const posts = await Post.find()
-            .populate({ path: "author", select: "username, profilePicture" })
-            .populate({
-                path: "comments",
-                sort: { createdAt: -1 },
-                populate: { path: "author", select: "username, profilePicture" }
-            });
         return res.status(200).json({
             success: true,
-            message: "Posts fetched successfully!",
-            posts
+            message: "New Post added successfully!",
+            post
         });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to fetch posts!"
-        });
-    }
-}
 
-export const getUserPosts = async (req, res) => {
-    try {
-        const authorId = req.id;
-        const posts = await Post.find({ author: authorId }).sort({ createdAt: -1 }).populate({ path: "author", select: "username, profilePicture" }).populate({ path: "comments", sort: { createdAt: -1 }, populate: { path: "author", select: "username, profilePicture" } });
-        return res.status(200).json({
-            success: true,
-            message: "User's posts fetched successfully!",
-            posts
-        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({
             success: false,
-            message: "Failed to fetch user's posts!"
+            message: "Failed to add new post!"
         });
     }
 }
