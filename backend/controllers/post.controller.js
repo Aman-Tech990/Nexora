54669960f550098a0cfde377a88e3bd47b8fc3b5
Aman Parida <amantechz467@gmail.com>
2025-09-16 22:12:43 +0530

    Post controller fully fixed!

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -130,7 +130,7 @@ export const likePost = async (req, res) => {
         console.log(error);
         return res.status(500).json({
             success: false,
-            message: "Failed to like posts!"
+            message: "Failed to like post!"
         });
     }
 }
